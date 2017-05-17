b6646d099d5a1c3c3cbe8f0f94cd74d3a822e877
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-17 15:40:14 -0700

    mongodb sdk

---
+++ index.js
@@ -0,0 +1,15 @@
+var config  = require('config')
+  , Mongo   = require('./lib/index')
+  ;
+
+class SDK {
+  constructor(options = {}) {
+    let { bearer } = options;
+
+    this.mongodb = new Mongo({ bearer });
+  }
+}
+
+module.exports = {
+  sdk : (...args) => new SDK(...args);
+}
